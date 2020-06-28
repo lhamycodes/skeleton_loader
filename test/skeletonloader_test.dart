9d3c534cc45e9e4920e10cc7aa75e9e3f224d7a6
Adedeji Rotibi <dayjeerow7@gmail.com>
2020-06-28 17:39:26 +0100

    Add example project

--- test/skeletonloader_test.dart
+++ test/skeletonloader_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:skeleton_loader/skeleton_loader.dart';
+// import 'package:skeleton_loader/skeleton_loader.dart';
 
 void main() {
   test('adds one to input values', () {
